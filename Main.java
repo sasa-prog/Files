4d3ac1aa91458e113d88ab9a5462771ceb5133a0
sasa05 <74849601+sasa-prog@users.noreply.github.com>
2022-01-21 16:12:14 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
-package io.github.sasaprog;
-
 import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.net.*;
-//Started a Strings Find and Computing
+
 public class Main {
     public static void main(String[] args){
         System.out.print("\n検索キーワード>>>");
